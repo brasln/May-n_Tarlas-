ea43c4f01c510c13ea7c067b69da2f939860081c
brasln <beraaslan00@gmail.com>
2023-11-28 18:27:16 +0300

    Mayin_Tarlasi

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace MayinTarlasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radio_zor = new System.Windows.Forms.RadioButton();
+            this.radio_orta = new System.Windows.Forms.RadioButton();
+            this.radio_kolay = new System.Windows.Forms.RadioButton();
+            this.btn_oyna = new System.Windows.Forms.Button();
+            this.lbl_puan = new System.Windows.Forms.Label();
+            this.lbl_mayin_sayisi = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 8;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 5);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 8;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(529, 523);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.AccessibleDescription = "";
+            this.panel1.AccessibleName = "";
+            this.panel1.Controls.Add(this.radio_zor);
+            this.panel1.Controls.Add(this.radio_orta);
+            this.panel1.Controls.Add(this.radio_kolay);
+            this.panel1.Location = new System.Drawing.Point(588, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 124);
+            this.panel1.TabIndex = 1;
+            this.panel1.Tag = "";
+            // 
+            // radio_zor
+            // 
+            this.radio_zor.AutoSize = true;
+            this.radio_zor.Font = new System.Drawing.Font("Perpetua Titling MT", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radio_zor.ForeColor = System.Drawing.Color.BurlyWood;
+            this.radio_zor.Location = new System.Drawing.Point(16, 83);
+            this.radio_zor.Name = "radio_zor";
+            this.radio_zor.Size = new System.Drawing.Size(56, 19);
+            this.radio_zor.TabIndex = 2;
+            this.radio_zor.TabStop = true;
+            this.radio_zor.Text = "Zor";
+            this.radio_zor.UseVisualStyleBackColor = true;
+            this.radio_zor.CheckedChanged += new System.EventHandler(this.radio_zor_CheckedChanged);
+            // 
+            // radio_orta
+            // 
+            this.radio_orta.AutoSize = true;
+            this.radio_orta.Font = new System.Drawing.Font("Perpetua Titling MT", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radio_orta.ForeColor = System.Drawing.Color.BurlyWood;
+            this.radio_orta.Location = new System.Drawing.Point(16, 57);
+            this.radio_orta.Name = "radio_orta";
+            this.radio_orta.Size = new System.Drawing.Size(66, 19);
+            this.radio_orta.TabIndex = 1;
+            this.radio_orta.TabStop = true;
+            this.radio_orta.Text = "Orta";
+            this.radio_orta.UseVisualStyleBackColor = true;
+            this.radio_orta.CheckedChanged += new System.EventHandler(this.radio_orta_CheckedChanged);
+            // 
+            // radio_kolay
+            // 
+            this.radio_kolay.AutoSize = true;
+            this.radio_kolay.Font = new System.Drawing.Font("Perpetua Titling MT", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radio_kolay.ForeColor = System.Drawing.Color.BurlyWood;
+            this.radio_kolay.Location = new System.Drawing.Point(16, 30);
+            this.radio_kolay.Name = "radio_kolay";
+            this.radio_kolay.Size = new System.Drawing.Size(73, 19);
+            this.radio_kolay.TabIndex = 0;
+            this.radio_kolay.TabStop = true;
+            this.radio_kolay.Text = "Kolay";
+            this.radio_kolay.UseVisualStyleBackColor = true;
+            this.radio_kolay.CheckedChanged += new System.EventHandler(this.radio_kolay_CheckedChanged);
+            // 
+            // btn_oyna
+            // 
+            this.btn_oyna.BackColor = System.Drawing.Color.Tan;
+            this.btn_oyna.Font = new System.Drawing.Font("Perpetua Titling MT", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_oyna.ForeColor = System.Drawing.Color.Brown;
+            this.btn_oyna.Location = new System.Drawing.Point(588, 170);
+            this.btn_oyna.Name = "btn_oyna";
+            this.btn_oyna.Size = new System.Drawing.Size(200, 54);
+            this.btn_oyna.TabIndex = 2;
+            this.btn_oyna.Text = "OYNA";
+            this.btn_oyna.UseVisualStyleBackColor = false;
+            this.btn_oyna.Click += new System.EventHandler(this.btn_oyna_Click);
+            // 
+            // lbl_puan
+            // 
+            this.lbl_puan.AutoSize = true;
+            this.lbl_puan.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_puan.ForeColor = System.Drawing.Color.BurlyWood;
+            this.lbl_puan.Location = new System.Drawing.Point(585, 282);
+            this.lbl_puan.Name = "lbl_puan";
+            this.lbl_puan.Size = new System.Drawing.Size(70, 23);
+            this.lbl_puan.TabIndex = 3;
+            this.lbl_puan.Text = "PUAN:";
+            // 
+            // lbl_mayin_sayisi
+            // 
+            this.lbl_mayin_sayisi.AutoSize = true;
+            this.lbl_mayin_sayisi.Location = new System.Drawing.Point(588, 231);
+            this.lbl_mayin_sayisi.Name = "lbl_mayin_sayisi";
+            this.lbl_mayin_sayisi.Size = new System.Drawing.Size(16, 16);
+            this.lbl_mayin_sayisi.TabIndex = 4;
+            this.lbl_mayin_sayisi.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkBlue;
+            this.ClientSize = new System.Drawing.Size(804, 550);
+            this.Controls.Add(this.lbl_mayin_sayisi);
+            this.Controls.Add(this.lbl_puan);
+            this.Controls.Add(this.btn_oyna);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Mayın Tarlası";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radio_zor;
+        private System.Windows.Forms.RadioButton radio_orta;
+        private System.Windows.Forms.RadioButton radio_kolay;
+        private System.Windows.Forms.Button btn_oyna;
+        private System.Windows.Forms.Label lbl_puan;
+        private System.Windows.Forms.Label lbl_mayin_sayisi;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace MayinTarlasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        List<int> mayın = new List<int>();
+        int puan, dtiklanan = 0, mayinsayisi = 0, kutu = 0;
+
+        private void radio_kolay_CheckedChanged(object sender, EventArgs e)
+        {
+            if(radio_kolay.Checked==true)
+            {
+                lbl_mayin_sayisi.Text = "Mayın Sayısı 10";
+            }
+        }
+
+        private void radio_orta_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radio_orta.Checked == true)
+            {
+                lbl_mayin_sayisi.Text = "Mayın Sayısı 20";
+            }
+        }
+
+        private void radio_zor_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radio_zor.Checked == true)
+            {
+                lbl_mayin_sayisi.Text = "Mayın Sayısı 30";
+            }
+        }
+
+        
+
+        private void btn_oyna_Click(object sender, EventArgs e)
+        {
+            if (radio_kolay.Checked == true)
+                Oyna("kolay");
+            else if (radio_orta.Checked == true)
+                Oyna("orta");
+            else if (radio_zor.Checked == true)
+                Oyna("zor");
+            else
+                MessageBox.Show("Zorluk Seçin");
+        }
+
+        private void Oyna(string mod)
+        {
+            tableLayoutPanel1.ColumnStyles.Clear();
+            tableLayoutPanel1.RowStyles.Clear();
+            tableLayoutPanel1.Controls.Clear();
+            mayın.Clear();
+            dtiklanan = 0;
+            mayinsayisi = 0;
+            kutu = 0;
+            puan = 0;
+            lbl_puan.Text = "PUAN: 0";
+
+            if (mod == "kolay") mayinsayisi = 10;
+            else if (mod == "orta") mayinsayisi = 25;
+            else if (mod == "zor") mayinsayisi = 40;
+            for (int i = 0; i < tableLayoutPanel1.ColumnCount; i++)
+            {
+                tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+
+                for (int x = 0; x < tableLayoutPanel1.RowCount; x++)
+                {
+                    if (i == 0)
+                    {
+                        tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+                    }
+                    Button cmd = new Button();
+                    cmd.BackColor = Color.Gray;
+                    cmd.Dock = DockStyle.Fill;
+                    cmd.Name = kutu.ToString();
+                    tableLayoutPanel1.Controls.Add(cmd, i, x);
+                    kutu++;
+                }
+            }
+            int randomsayi;
+            for (int i = 0; i < mayinsayisi; i++)
+            {
+                do
+                {
+                    randomsayi = rnd.Next(1, (tableLayoutPanel1.ColumnCount * tableLayoutPanel1.RowCount) - 1);
+
+                } while (mayın.Contains(randomsayi));
+
+                mayın.Add(randomsayi);
+            }
+            ButtonClickAyarla();
+        }
+        private void ButtonClickAyarla()
+        {
+            foreach (Control ctl in this.tableLayoutPanel1.Controls)
+            {
+                ctl.MouseClick += new MouseEventHandler(Form1_MouseClick);
+            }
+        }
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Event(sender);
+        }
+        private void Event(object sender)
+        {
+            if (sender.GetType().Name == "Button")
+            {
+                Button tiklanan = (Button)sender;
+                if (tiklanan.BackColor != Color.Green && tiklanan.BackColor != Color.Red)
+                {
+                    string isim = tiklanan.Name;
+                    if (mayın.IndexOf(Convert.ToInt32(isim)) != -1) // Yandın
+                    {
+                        tiklanan.BackColor = Color.Red;
+                        tiklanan.Image = Image.FromFile((@"C:\Users\Bera\Documents\Visual Studio 2022\Projects\MayinTarlasi\Pictures\mine.png"));
+                        HepsiniAc();
+                        MessageBox.Show("Kaybettiniz. \nToplam Puan: " + puan);
+                    }
+                    else // Kazandın
+                    {
+                        tiklanan.BackColor = Color.Green;
+                        Random rnd = new Random();
+                        int tikpuan = rnd.Next(1, mayinsayisi);
+                        puan += tikpuan;
+                        //tiklanan.Text = tikpuan.ToString();
+                        lbl_puan.Text = "PUAN: " + puan.ToString();
+
+                        if (dtiklanan == ((tableLayoutPanel1.ColumnCount * tableLayoutPanel1.RowCount) - 1) - mayinsayisi)
+                        {
+                            HepsiniAc();
+                            MessageBox.Show("Kazandınız \nToplam Puan: " + puan);
+                        }
+                        else
+                        {
+                            dtiklanan++;
+                        }
+                    }
+                }
+            }
+        }
+        private void HepsiniAc()
+        {
+            for (int i = 0; i <= (tableLayoutPanel1.ColumnCount * tableLayoutPanel1.RowCount) - 1; i++)
+            {
+                Button btn = (Button)tableLayoutPanel1.Controls[i];
+                if (mayın.IndexOf(Convert.ToInt32(btn.Name)) != -1)
+                {
+                    btn.BackColor = Color.Red;
+                    btn.BackgroundImageLayout = ImageLayout.Stretch;
+                    btn.BackgroundImage = Image.FromFile((@"C:\Users\Bera\Documents\Visual Studio 2022\Projects\MayinTarlasi\Pictures\mine.png"));
+                }
+                else
+                {
+                    btn.BackColor = Color.Green;
+                }
+            }
+        }
+    }
+}
